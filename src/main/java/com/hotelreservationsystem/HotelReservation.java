83b294511745004125ccf185ed72a3347f2658a8
shaheen5 <miyashaheen1995@gmail.com>
2021-04-26 09:25:24 +0530

    Added : Code To Display Welcome Message

---
+++ src/main/java/com/hotelreservationsystem/HotelReservation.java
@@ -0,0 +1,8 @@
+package com.hotelreservationsystem;
+
+public class HotelReservation {
+    public static void main(String[] args) {
+        //display welcome message
+        System.out.println("Welcome To Hotel Reservation Program !");
+    }
+}
